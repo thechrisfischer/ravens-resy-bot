10113e98a07fc159fa1c7377c6b605e6ce40d500
Chris Fischer <chris@fischer.nyc>
2024-02-29 13:53:58 -0500

    moving scheduled jobs to their own clock process

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     def __init__(self):
         self.RESY_USER_CONFIG = os.getenv("RESY_USER_CONFIG")
         self.SLACK_URL = os.getenv("SLACK_URL")
+        self.RESERVATION_CONFIG_PATH = os.getenv("RESERVATION_CONFIG_PATH")
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import argparse
 import json
 import dateparser
 import datetime
 import os
-
-import resy_bot.scheduler as ap_scheduler
 
 from config import Config
 
@@ -17,52 +14,14 @@ from flask import Flask, request, Response, render_template
 config = Config()
 
 RESY_USER_CONFIG = config.RESY_USER_CONFIG
+RESERVATION_CONFIG_PATH = config.RESERVATION_CONFIG_PATH
 
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
 app = Flask(__name__)
 
-scheduler = ap_scheduler.initialize()
-
 slogger = Slogger()
-
-## initialize scheduler
-#scheduler = APScheduler()
-#scheduler.api_enabled = True
-#scheduler.init_app(app)
-#scheduler.start()
-
-### TODO create function to load from new config files
-def load_reservations(reservation_config_path: str) -> str:
-    logger.info("loading reservation requests")
-    
-    config_data = json.loads(RESY_USER_CONFIG)
-
-    with open(reservation_config_path, "r") as f:
-        reservation_data = json.load(f)
-
-    config = ResyConfig(**config_data)
-    manager = ResyManager.build(config)
-
-    print("checking scheduled reservations")
-    scheduled_reservations = reservation_data["scheduled"]
-    restaurants = scheduled_reservations.keys()
-    job_ids = []
-    for r in restaurants:
-        reservation_request = scheduled_reservations[r]
-        logger.info(reservation_request)
-        logger.info(f"Making a scheduled reservation drop for {reservation_request}")
-        timed_request = TimedReservationRequest(**reservation_request)
-        scheduler.add_job(manager.make_reservation_at_opening_time, args=[timed_request], trigger="cron", hour="7", id=r, replace_existing=True)
-        job_ids.append(r)
-    
-    return job_ids
-
-    #timed_request = TimedReservationRequest(**reservation_data)
-
-    #return manager.make_reservation_at_opening_time(timed_request)
-
 
 def get_waitlisted_table(resy_config_path: str, reservation_config_path: str, 
                          notification):
@@ -99,20 +58,18 @@ def respond():
     data = notification.split("|")
     notification = [x.strip(" \n") for x in data]
     
-    get_waitlisted_table(RESY_USER_CONFIG, "reservation_configs/reservations.json", notification)
+    get_waitlisted_table(RESY_USER_CONFIG, RESERVATION_CONFIG_PATH, notification)
     return Response(status=200)
 
-@app.route('/scheduled-jobs')
-def get_scheduled_jobs():
-    jobs = scheduler.get_jobs()
-
-    for j in jobs:
-        print(f"{j.id} ---- {j.next_run_time}")
-        
-    return render_template("index.html", jobs=scheduler.get_jobs())
+###@app.route('/scheduled-jobs')
+###def get_scheduled_jobs():
+###    jobs = scheduler.get_jobs()
+###
+###    for j in jobs:
+###        print(f"{j.id} ---- {j.next_run_time}")
+###        
+###    return render_template("index.html", jobs=scheduler.get_jobs())
 
 if __name__ == "__main__":
 
-    ### wait_for_drop_time(args.resy_config_path, args.reservation_config_path)
-    load_reservations("reservation_configs/reservations.json")
     app.run(debug = True, host = "0.0.0.0", port = 3000)
